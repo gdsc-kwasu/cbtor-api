fe431e86f70670a29d63db5d24e64c53dfe24e4c
wolvecode <wolvecode@gmail.com>
2020-12-22 08:44:44 +0100

    change code architecture

--- src/app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const app = express()
-const route = require('./routes/user.route')
-
-app.use('/', route)
-
-app.listen(3300, () => {
-  console.log(`Listening on port ${3300}`)
-})
