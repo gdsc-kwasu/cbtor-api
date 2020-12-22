630725edfc8c9cfe6b794e4150a20c67cee09a88
wolvecode <wolvecode@gmail.com>
2020-12-22 08:42:38 +0100

    change port

--- src/index.js
+++ src/index.js
@@ -1,4 +1,25 @@
-// Database connection.
-// Configuration setup.
+const express = require('express')
 
-// Start your app.
+const bodyParser = require('body-parser')
+
+const dotenv = require('dotenv')
+
+const {postUser} = require('./controller/users')
+
+const makeCallback = require('./express-callback')
+
+dotenv.config()
+
+const app = express()
+app.use(bodyParser.json())
+// TODO: figure out DNT compliance.
+app.use((_, res, next) => {
+  res.set({Tk: '!'})
+  next()
+})
+app.post('/users', makeCallback(postUser))
+
+// listen for requests
+app.listen(4000, () => {
+  console.log('Server is listening on port 4000')
+})
