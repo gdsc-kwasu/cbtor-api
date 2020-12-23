c7a4405b6ec368d0b0fbffee71ebe40a5bdf4c30
wolvecode <wolvecode@gmail.com>
2020-12-23 14:42:03 +0100

    user get all user and get user by email, by id.

--- src/controller/users/index.js
+++ src/controller/users/index.js
@@ -1,12 +1,18 @@
-const addUser = require('../../use-cases/user')
+const {addUser, listUsers, userById} = require('../../use-cases/user')
 
 const makePostUser = require('./post-user')
+const makeGetUsers = require('./get-user')
+const makeGetUserId = require('./get-user-id')
 
 const postUser = makePostUser({addUser})
+const getUser = makeGetUsers({listUsers})
+const getUserId = makeGetUserId({userById})
 
 const userController = Object.freeze({
   postUser,
+  getUser,
+  getUserId,
 })
 
 module.exports = userController
-module.exports = {postUser}
+module.exports = {postUser, getUser, getUserId}
